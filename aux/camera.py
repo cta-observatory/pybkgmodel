6a6cf95eeb64bce857c4141078a909b3a3d31222
Ievgen Vovk <vovk@icrr.u-tokyo.ac.jp>
2022-02-07 13:32:37 +0900

    CameraImage: masking and center information.

--- aux/camera.py
+++ aux/camera.py
@@ -1,24 +1,45 @@
 import numpy
-from astropy.coordinates import SkyCoord
+import numpy.ma
+
+from astropy.coordinates import SkyCoord, Angle
 from matplotlib import pyplot
 
 
 class CameraImage:
-    def __init__(self, image, xedges, yedges, energy_edges):
+    def __init__(self, image, xedges, yedges, energy_edges, center=None):
         self.image = image
         self.xedges = xedges
         self.yedges = yedges
         self.energy_edges = energy_edges
-        
+        self.center = center
+
+        x = (xedges[1:] + xedges[:-1]) / 2
+        y = (yedges[1:] + yedges[:-1]) / 2
+
+        xx, yy = numpy.meshgrid(x, y, indexing='ij')
+
+        if self.center is None:
+            frame=None
+        else:
+            frame=self.center.skyoffset_frame()
+
+        self.pixel_coords = SkyCoord(
+            xx,
+            yy,
+            frame=frame
+        )
+
     @classmethod
     def from_events(cls, event_file, xedges, yedges, energy_edges):
+        center = cls.get_poiting(event_file)
         image = cls.bin_events(event_file, xedges, yedges, energy_edges)
-        
-        return CameraImage(image, xedges, yedges, energy_edges)
-        
+
+        return CameraImage(image, xedges, yedges, energy_edges, center=center)
+
     def __repr__(self):
         print(
 f"""{type(self).__name__} instance
+    {'Center':.<20s}: {self.center}
     {'X range':.<20s}: [{self.xedges.min():.1f}, {self.xedges.max():.1f}]
     {'Y range':.<20s}: [{self.yedges.min():.1f}, {self.yedges.max():.1f}]
     {'X bins':.<20s}: {len(self.xedges) - 1}
@@ -27,11 +48,35 @@ f"""{type(self).__name__} instance
         )
 
         return super().__repr__()
-        
+
     @classmethod
-    def bin_events(self, event_file, xedges, yedges, energy_edges):
+    def bin_events(cls, event_file, xedges, yedges, energy_edges):
         pass
-    
+
+    @classmethod
+    def get_poiting(cls, event_file):
+        return SkyCoord(ra=event_file.pointing_ra.mean(), dec=event_file.pointing_dec.mean())
+
+    def remove_mask(self):
+        self.image.mask = False
+
+    def mask_half(self, pointer):
+        offset_delta = Angle('90d')
+
+        pixel_position_angles = self.center.position_angle(self.pixel_coords)
+        pointer_position_angle = self.center.position_angle(pointer)
+        position_angle_offest = (pixel_position_angles - pointer_position_angle).wrap_at('180d')
+
+        to_mask = (position_angle_offest >= -offset_delta) & (position_angle_offest < offset_delta)
+
+        self.image = numpy.ma.masked_where(to_mask[None, ...], self.image)
+
+        return position_angle_offest
+
+    def mask_circle(self, center, rad):
+        to_mask = center.separation(self.pixel_coords) < rad
+        self.image = numpy.ma.masked_where(to_mask[None, ...], self.image)
+
     def plot(self, energy_bin_id=0, unit='deg', **kwargs):
         pyplot.xlabel(f'X [{unit}]')
         pyplot.ylabel(f'Y [{unit}]')
@@ -45,12 +90,12 @@ f"""{type(self).__name__} instance
 
 class RectangularCameraImage(CameraImage):
     @classmethod
-    def bin_events(self, event_file, xedges, yedges, energy_edges):
-        pointing = SkyCoord(ra=event_file.pointing_ra, dec=event_file.pointing_dec)
+    def bin_events(cls, event_file, xedges, yedges, energy_edges):
+        center = cls.get_poiting(event_file)
         events = SkyCoord(ra=event_file.event_ra, dec=event_file.event_dec)
-        
-        cam = events.transform_to(pointing.skyoffset_frame())
-        
+
+        cam = events.transform_to(center.skyoffset_frame())
+
         hist, _ = numpy.histogramdd(
             sample=(
                 event_file.event_energy,
@@ -63,5 +108,5 @@ class RectangularCameraImage(CameraImage):
                 yedges
             )
         )
-        
+
         return hist
